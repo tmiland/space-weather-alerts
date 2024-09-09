8f730ff06c1aacbca7d3ead4c6e703112e0932de
Tommy Miland <kontakt@tmiland.com>
2024-09-09 21:45:45 +0200

    Update swa.sh

--- swa.sh
+++ swa.sh
@@ -170,6 +170,7 @@ install() {
 
   download_files() {
     if [[ $(command -v 'curl') ]]; then
+      mkdir -p "${swa_folder}"/assets
       curl -fsSLk "$swa_url" > "${swa_folder}"/swa.sh
       curl -fsSLk "$swa_service" > "$systemd_user_folder"/swa.service
       for p in G1.png G2.png G3.png G4.png G5.png R1.png R2.png R3.png R4.png R5.png S1.png S2.png S3.png S4.png S5.png
@@ -177,6 +178,7 @@ install() {
         curl -fsSLk https://github.com/tmiland/Space-Weather-Alerts/tree/main/assets/$p > "${swa_folder}"/assets/$p
       done
     elif [[ $(command -v 'wget') ]]; then
+      mkdir -p "${swa_folder}"/assets
       for p in G1.png G2.png G3.png G4.png G5.png R1.png R2.png R3.png R4.png R5.png S1.png S2.png S3.png S4.png S5.png
       do
         wget -q https://github.com/tmiland/Space-Weather-Alerts/tree/main/assets/$p -O "${swa_folder}"/assets/$p
